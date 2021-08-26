66b69be6218997368a569296e2c74afaa9da94c1
milkinhale <62606734+milkinhale@users.noreply.github.com>
2021-08-26 17:48:07 +0900

    시나리오
    시나리오.TXT

--- src/team3_1st_project/GitTest02.java
+++ src/team3_1st_project/GitTest02.java
@@ -4,6 +4,8 @@ public class GitTest02 {
 
 	public static void main(String[] args) {
 		System.out.println("게임하고 싶어요");
+		System.out.println("나 마스터였는데 얼마전에 빠대하다가 시즌 20 마스터 떨굴까봐 더이상 경쟁 안돌렸다고 무시당함...");
+		System.out.println("개빡쳐서 1:1 매치로 참교육 시켜줄려고 했는데 친추도 안받고 차단박혀서 혼자 씩씩거리다가 잠...");
 	}
 
 }
