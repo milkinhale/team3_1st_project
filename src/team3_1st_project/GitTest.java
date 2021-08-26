a034c06ca11341aae0c7e37d614b757d7eac0017
milkinhale <62606734+milkinhale@users.noreply.github.com>
2021-08-26 16:30:55 +0900

    Initial commit

---
+++ src/team3_1st_project/GitTest.java
@@ -0,0 +1,12 @@
+package team3_1st_project;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("자고싶다");
+
+	}
+
+}
