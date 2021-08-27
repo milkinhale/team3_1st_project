d6b49dca398a352c5a999d076368bcb2346f2b52
milkinhale <62606734+milkinhale@users.noreply.github.com>
2021-08-27 16:46:56 +0900

    Update OrderDAO.java

--- src/kosta/mvc/model/dao/OrderDAO.java
+++ src/kosta/mvc/model/dao/OrderDAO.java
@@ -38,6 +38,7 @@ public interface OrderDAO {
 	   *  1) orders테이블에 insert
 	   *  2) order_line테이블에 insert
 	   *  3) 재고량(stock)감소 시키기(update)
+	   * 	
 	   * 	구매자 메뉴에서 사용함.
 	   * 	막판에 쿠폰 체크하는거 잊지 말기! 
 	   * 
