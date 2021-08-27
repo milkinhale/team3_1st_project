0e3265d6f82e4d46f521e367237fed4b3ebc2776
sunmin_hwang <tommy08190@naver.com>
2021-08-27 11:07:19 +0900

    util

---
+++ src/util/DBUtil.java
@@ -0,0 +1,69 @@
+package util;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+/**
+ * DB연동을 위한 로드, 연결, 닫기
+ */
+public class DBUtil {
+	private static Properties proFile = new Properties();
+	
+	public static Properties getProFile() {
+		return proFile;
+	}
+	
+	/**
+	 * 로드
+	 */
+	static {
+		try {
+			proFile.load(new FileInputStream("resources/dbInfo.properties"));
+		
+			Class.forName(proFile.getProperty("driverName")); // 오라클 드라이버를 찾는다.
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 연결
+	 */
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(proFile.getProperty("url"), proFile.getProperty("userName"), proFile.getProperty("userPass"));
+	}
+
+	/**
+	 * 닫기(select인 경우)
+	 */
+	public static void dbClose(Connection con, Statement st, ResultSet rs) {
+		try {
+			if (rs != null)	rs.close();
+			dbClose(con, st);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * 닫기(DML, DDL인 경우)
+	 */
+	public static void dbClose(Connection con, Statement st) {
+
+		try {
+			if (st != null)
+				st.close();
+			if (con != null)
+				con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
