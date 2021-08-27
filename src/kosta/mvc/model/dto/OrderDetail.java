2dcb00e6d9778efa85587057169c0a86290fa276
milkinhale <62606734+milkinhale@users.noreply.github.com>
2021-08-27 14:46:40 +0900

    dto
    ad

---
+++ src/kosta/mvc/model/dto/OrderDetail.java
@@ -0,0 +1,68 @@
+package kosta.mvc.model.dto;
+
+public class OrderDetail {
+	private int orderDetailNo;
+	private int liquorNo;
+	private int orderNo;
+	private int count;
+	private int orderPrice;
+	public OrderDetail() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public OrderDetail(int orderDetailNo, int liquorNo, int orderNo, int count, int orderPrice) {
+		super();
+		this.orderDetailNo = orderDetailNo;
+		this.liquorNo = liquorNo;
+		this.orderNo = orderNo;
+		this.count = count;
+		this.orderPrice = orderPrice;
+	}
+	public int getOrderDetailNo() {
+		return orderDetailNo;
+	}
+	public void setOrderDetailNo(int orderDetailNo) {
+		this.orderDetailNo = orderDetailNo;
+	}
+	public int getLiquorNo() {
+		return liquorNo;
+	}
+	public void setLiquorNo(int liquorNo) {
+		this.liquorNo = liquorNo;
+	}
+	public int getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getOrderPrice() {
+		return orderPrice;
+	}
+	public void setOrderPrice(int orderPrice) {
+		this.orderPrice = orderPrice;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("OrderDetail [orderDetailNo=");
+		builder.append(orderDetailNo);
+		builder.append(", liquorNo=");
+		builder.append(liquorNo);
+		builder.append(", orderNo=");
+		builder.append(orderNo);
+		builder.append(", count=");
+		builder.append(count);
+		builder.append(", orderPrice=");
+		builder.append(orderPrice);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+}
