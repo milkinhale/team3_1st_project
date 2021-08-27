656356376708fe82604268c5d608a0e449d5f275
salientsoph <69945903+salientsoph@users.noreply.github.com>
2021-08-28 00:56:59 +0900

    liquor commit

--- src/kosta/mvc/model/dao/LiquorDAO.java
+++ src/kosta/mvc/model/dao/LiquorDAO.java
@@ -10,37 +10,41 @@ public interface LiquorDAO {
 		List<Liquor> goodsSelect() throws SQLException;
 	*/
 	
-	
 	/**
 	 * 가격대별 검색 
 	 * */
-	List<Liquor> goodsSelectByGoodsPrice(int cost) throws SQLException;
+	List<Liquor> liquorsSelectByLiquorPrice(int cost) throws SQLException;
 	
 	/**
 	 * 종류별 검색 
 	 * */
-	List<Liquor> goodsSelectByGoodsKind(String liqourType) throws SQLException;
+	List<Liquor> liquorsSelectByLiquorType(String liqourType) throws SQLException;
+	
 	
 	/**
-	 * 이름에 해당하는 정보 검색
+	 * 번호(PK)에 해당하는 정보 검색
 	 * */
-	Liquor goodsSelectByGoodsName(String liqourName) throws SQLException;
+	Liquor liquorSelectByLiquorNo(int liquorNo) throws SQLException;
 		
 		
 	/**
 	 * 양주 등록하기 
 	 * */
-	int insertGoods(Liquor liquorDTO) throws SQLException;
+	int insertLiquor(Liquor liquorDTO) throws SQLException;
 	
 		
 	/**
 	 * 양주 수정하기 
 	 * */
-	int updateGoods(Liquor liquorDTO) throws SQLException;
-		
+	int updateLiquor(Liquor liquorDTO) throws SQLException;
+	
+	/**
+	 * 재고 수량 바꾸기 
+	 * */
+	int updateStock(int count, int liquorNo) throws SQLException;
 		
 	/**
 	 * 양주 삭제하기 
 	 * */
-	int deleteGoods(int liquorNo) throws SQLException;
+	int deleteLiquor(int liquorNo) throws SQLException;
 }
