08f06e2e75312d9d70f965cbf5192ac223803fba
salientsoph <69945903+salientsoph@users.noreply.github.com>
2021-08-29 16:14:35 +0900

    liquor contoller&endview commit

--- src/kosta/mvc/controller/LiquorController.java
+++ src/kosta/mvc/controller/LiquorController.java
@@ -1,22 +1,76 @@
 package kosta.mvc.controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import kosta.mvc.exception.NotFoundException;
+import kosta.mvc.model.dto.Liquor;
 import kosta.mvc.model.service.LiquorService;
+import kosta.mvc.view.FailView;
+import kosta.mvc.view.LiquorEndView;
+
 
 public class LiquorController {
-	static LiquorService goodsService = new LiquorService();
-  /**
-   * 전체 상품 조회
-   * */
-	/*public static void goodsSelect() {
+	static LiquorService liquorService = new LiquorService();
+	
+	public static void liquorsSelectByLiquorPrice(int price) {
 		try {
-			List<Goods> list = goodsService.goodsSelect();
-			EndView.printGoodsList(list);
-		}catch (Exception e) {
+			List<Liquor> liquorList = liquorService.liquorsSelectByLiquorPrice(price);
+			LiquorEndView.printLiquorList(liquorList);
+		} catch (Exception e) {
 			FailView.errorMessage(e.getMessage());
 		}
-	}*/
+	}
 	
+	public static void liquorsSelectByLiquorType(int liqourTableNo) {
+		try {
+			List<Liquor> liquorList = liquorService.liquorsSelectByLiquorType(liqourTableNo);
+			LiquorEndView.printLiquorList(liquorList);
+		} catch (Exception e) {
+			FailView.errorMessage(e.getMessage());
+		}
+	}
+	
+
+	
+	public static void liquorSelectByLiquorNo(int liquorNo){
+		try {
+			Liquor liquor = liquorService.liquorSelectByLiquorNo(liquorNo);
+			LiquorEndView.printLiquor(liquor);
+		}catch(SQLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
+	}
+
 	/**
 	 * 
 	 * */
+	public static void insertLiquor(Liquor liquorDTO) {
+		try {
+			liquorService.insertLiquor(liquorDTO);
+			LiquorEndView.messagePrint("상품이 등록되었어요.");
+		}catch(SQLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
+	}
+
+
+	public static void updateLiquor(Liquor liquorDTO) {
+		try {
+			liquorService.updateLiquor(liquorDTO);
+			LiquorEndView.messagePrint("상품이 삭제되었어요.");
+		}catch(SQLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
+	}
+
+	
+	public static void deleteLiquor(int liquorNo){
+		try {
+			liquorService.deleteLiquor(liquorNo);
+			LiquorEndView.messagePrint("상품이 삭제되었어요.");
+		}catch(SQLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
+	}
 }
--- src/kosta/mvc/view/LiquorEndView.java
+++ src/kosta/mvc/view/LiquorEndView.java
@@ -1,5 +1,28 @@
 package kosta.mvc.view;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import kosta.mvc.exception.NotFoundException;
+import kosta.mvc.model.dto.Liquor;
+
+
 public class LiquorEndView {
+	public static void printLiquorList(List<Liquor> liquorList) {
+		System.out.println("------해당 조건에 따라 " + liquorList.size() + "건의 상품이 조회되었어요.-----");
+		for (Liquor liquor : liquorList) {
+			System.out.println(liquor);
+		}
+		System.out.println();
+	}
+	
+	public static void printLiquor(Liquor liquorDTO) {
+		System.out.println("해당 상품 정보입니다.");
+		System.out.println(liquorDTO + "굈");
+	}
+	
+	public static void messagePrint(String message) {
+		System.out.println(message);
+	}
 
 }
